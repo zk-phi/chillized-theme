a4d37e58dda1325f47b7f646ed82b29ff18af7ad
p <_>
2017-03-29 11:40:39 +0900

    Fix copyright

--- chillized-theme.el
+++ chillized-theme.el
@@ -1,6 +1,6 @@
 ;;; chillized-theme.el --- a solarized-based dark chill theme
 
-;; Copyright (C) 2016- zk_phi
+;; Copyright (C) 2017- zk_phi
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
